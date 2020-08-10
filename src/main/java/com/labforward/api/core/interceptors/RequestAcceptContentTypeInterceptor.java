d37d927ced29ccb9b10d3f763160608c53a2657d
mohamedelkashif <mohamed_elkashif@windowslive.com>
2020-08-10 19:29:22 +0200

    Reorder the modifiers

--- src/main/java/com/labforward/api/core/interceptors/RequestAcceptContentTypeInterceptor.java
+++ src/main/java/com/labforward/api/core/interceptors/RequestAcceptContentTypeInterceptor.java
@@ -16,36 +16,36 @@ import java.util.stream.Collectors;
  */
 public class RequestAcceptContentTypeInterceptor extends HandlerInterceptorAdapter {
 
-	private final static String REQUIRED_HEADER_ACCEPT = "Accept";
+    private static final String REQUIRED_HEADER_ACCEPT = "Accept";
 
-	private final static String WILDCARD_ACCEPT_HEADER = "*/*";
+    private static final String WILDCARD_ACCEPT_HEADER = "*/*";
 
-	private final static String CONTENT_TYPE_JSON = "application/json";
+    private static final String CONTENT_TYPE_JSON = "application/json";
 
-	private static boolean isValidAcceptHeaderProvided(String headerValue) {
-		if (StringUtils.isEmpty(headerValue)) {
-			return true;
-		}
+    private static boolean isValidAcceptHeaderProvided(String headerValue) {
+        if (StringUtils.isEmpty(headerValue)) {
+            return true;
+        }
 
-		// extract single content type values,
-		// since header values can come chained up with additional requirement defined after colon, i.e.:
-		// text/html,application/xml;q=0.9,image/webp,image/png,*/*;q=0.8,application/signed-exchange;v=b3
-		Set<String> headerValues = Arrays.stream(headerValue.split(","))
-		                                 .map(val -> val.split(";")[0])
-		                                 .collect(Collectors.toSet());
+        // extract single content type values,
+        // since header values can come chained up with additional requirement defined after colon, i.e.:
+        // text/html,application/xml;q=0.9,image/webp,image/png,*/*;q=0.8,application/signed-exchange;v=b3
+        Set<String> headerValues = Arrays.stream(headerValue.split(","))
+                .map(val -> val.split(";")[0])
+                .collect(Collectors.toSet());
 
-		return (headerValues.contains(WILDCARD_ACCEPT_HEADER) || headerValues.contains(CONTENT_TYPE_JSON));
-	}
+        return (headerValues.contains(WILDCARD_ACCEPT_HEADER) || headerValues.contains(CONTENT_TYPE_JSON));
+    }
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
-		String contentType = request.getHeader(REQUIRED_HEADER_ACCEPT);
+        String contentType = request.getHeader(REQUIRED_HEADER_ACCEPT);
 
-		if (!isValidAcceptHeaderProvided(contentType)) {
-			throw new UnsupportedMediaTypeException();
-		}
+        if (!isValidAcceptHeaderProvided(contentType)) {
+            throw new UnsupportedMediaTypeException();
+        }
 
-		return true;
-	}
+        return true;
+    }
 }
