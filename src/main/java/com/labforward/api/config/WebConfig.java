163cdf0cfaab5006c96783dcc606ea738e18c31f
mohamedelkashif <mohamed_elkashif@windowslive.com>
2020-08-11 14:32:31 +0200

    config file for loogin interceptor

--- src/main/java/com/labforward/api/config/WebConfig.java
+++ src/main/java/com/labforward/api/config/WebConfig.java
@@ -1,4 +1,14 @@
 package com.labforward.api.config;
 
-public class WebConfig {
+import com.labforward.api.core.interceptors.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor()).addPathPatterns("/**");
+    }
 }
